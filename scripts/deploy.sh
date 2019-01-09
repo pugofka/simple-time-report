47ae71bb97504d0a3c9f802db8614920e48affc9
Валера Любимов <v.liubimov@pugofka.com>
2019-01-09 20:11:58 +0300

    fix(ci): занес токен форджа в env трэвиса и зашифровал

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting deployment"
 
-is_deployed=$(wget --server-response https://forge.laravel.com/servers/129626/sites/680296/deploy/http?token=04U4NrFHgtB5Oz8ENtA248gG9ZALITIQgfrfcoxy -O ip-current 2>&1| grep -c 'HTTP/1.1 200 OK')
+is_deployed=$(wget --server-response https://forge.laravel.com/servers/129626/sites/680296/deploy/http?token=$FORGE_TOKEN -O ip-current 2>&1| grep -c 'HTTP/1.1 200 OK')
 
 if [ $is_deployed == 1 ]; then
     echo "Deployed successfully"
