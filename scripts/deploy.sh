f53cb2ab3888eacdc35f954b54524f2adcc165fc
Валера Любимов <v.liubimov@pugofka.com>
2019-01-10 13:01:43 +0300

    fix(ci): исправления зашифрованных данных
    Вариант: travis encrypt "FOO=BAR" --add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting deployment"
 
+echo $FORGE_TOKEN
+
 is_deployed=$(wget --server-response https://forge.laravel.com/servers/129626/sites/680296/deploy/http?token=$FORGE_TOKEN -O ip-current 2>&1| grep -c 'HTTP/1.1 200 OK')
 
 if [ $is_deployed == 1 ]; then
