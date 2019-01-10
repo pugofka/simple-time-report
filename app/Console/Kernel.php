53c9ad3c8dc996ac503a5efaba8d0f6e33f34ad4
Valery Liubimov <v.udav@bk.ru>
2019-01-10 17:12:50 +0300

    fix: поменял время создания задачи
    На каждую пятницу в 17:00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('report:create')->weeklyOn(5, '8:00');
+        $schedule->command('report:create')->weeklyOn(5, '17:00');
     }
 
     /**
