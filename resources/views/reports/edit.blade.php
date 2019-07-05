90cf7d6c1feb99a915aa6a72b69a829a2cc917ca
Karmov <a.karmov@pugofka.com>
2019-07-05 13:59:22 +0300

    feat: видимость период отчета в режиме редактирования

--- resources/views/reports/edit.blade.php
+++ resources/views/reports/edit.blade.php
@@ -5,9 +5,11 @@
         <div class="col-12">
             <a href="{{route('reports.index')}}" class="btn btn-dark">Назад</a>
         </div>
-        
+
+
         <div class="col-12 mt-5 mb-5">
             <h2>Редактирование отчета</h2>
+            <p class="m-0">{{$report->report_start_date}} - {{$report->report_end_date}}</p>
         </div>
 
         <div class="col-12 p-0">
